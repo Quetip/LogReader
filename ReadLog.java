4219c826bfe5b67b5c88f0fc44f2f30651c5e404
Quetip <wroussel8365@gmail.com>
2020-11-01 18:30:18 -0500

    Update ReadLog.java

--- ReadLog.java
+++ ReadLog.java
@@ -12,7 +12,7 @@ public class ReadLog {
 
         Player player1 = new Player("test");
 
-        File f1=new File("C:\\Users\\wrous\\AppData\\Roaming\\.minecraft\\logs\\latest.log"); //Creation of File Descriptor for input file
+        File f1=new File("C:\\Users\\<user>\\AppData\\Roaming\\.minecraft\\logs\\latest.log"); //Creation of File Descriptor for input file
         String[] words= null;  //Intialize the word Array
         FileReader fr = new FileReader(f1);  //Creation of File Reader object
         BufferedReader br = new BufferedReader(fr); //Creation of BufferedReader object
@@ -47,23 +47,7 @@ public class ReadLog {
                  }
             }
         }
-//        ArrayList<String> player = new ArrayList<String>();
-//            String input="slain";   // Input word to be searched
-//            int count=0;   //Intialize the word to zero
-//            while((strLine=br.readLine())!=null)   //Reading Content from the file
-//            {
-//                words=strLine.split(" ");  //Split the word using space
-//                for (String word : words)
-//                {
-//                    if (word.equals(input))   //Search for the given word
-//                    {
-//                        System.out.println(strLine);
-//                        //add just the player
-//
-//                        count++;    //If Present increase the count by one
-//                    }
-//                }
-//            }
+//        
         if(count!=0)  //Check for count not equal to zero
         {
             System.out.println("Deaths: "+count);
